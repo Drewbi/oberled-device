ac54680259cb66ad9ccef5eacad18948bf26c965
Drew Alexander <drewalexander986@gmail.com>
2023-09-21 23:24:12 +0800

    add rust config

--- README.md
+++
@@ -1,6 +0,0 @@
-# OBERLED
-
-[IKEA Obergransad Wall lamp](https://www.ikea.com/au/en/p/obegraensad-led-wall-lamp-black-80526249/) controlled by an ESP32 running [toit](https://toit.io/).
-
-![oberleddemo1](https://user-images.githubusercontent.com/38243574/231802212-61b3b018-e44a-47e2-8104-dcb050a78ac2.gif)
-![oberleddemo2](https://user-images.githubusercontent.com/38243574/231801884-16993247-f09b-4454-b10f-4168491043ef.gif)
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
-use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
-use log::*;
+#![no_std]
+#![no_main]
 
-fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_sys::link_patches();
-    // Bind the log crate to the ESP Logging facilities
-    esp_idf_svc::log::EspLogger::initialize_default();
+use esp_backtrace as _;
+use esp_println::println;
+use hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, Delay, IO};
 
-    info!("Hello, world!");
+#[entry]
+fn main() -> ! {
+    let peripherals = Peripherals::take();
+    let system = peripherals.DPORT.split();
+    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+    
+    println!("Hello world!");
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    let mut led = io.pins.gpio7.into_push_pull_output();
+    
+    led.set_high().unwrap();
+    let mut delay = Delay::new(&clocks);
+
+    loop {
+        led.toggle().unwrap();
+        delay.delay_ms(500u32);
+    }
 }
