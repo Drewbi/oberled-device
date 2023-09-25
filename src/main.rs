9386690d684e3b6536aea6611da21d1fd697b43c
Drew Alexander <drewalexander986@gmail.com>
2023-09-25 12:27:18 +0800

    IT WORKS

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,44 @@
 
 use esp_backtrace as _;
 use esp_println::println;
-use hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, Delay, IO};
+use hal::{
+    clock::ClockControl,
+    peripherals::Peripherals,
+    prelude::*,
+    timer::TimerGroup,
+    Rtc,
+    Delay,
+    IO
+};
 
 #[entry]
 fn main() -> ! {
     let peripherals = Peripherals::take();
-    let system = peripherals.DPORT.split();
+    let mut system = peripherals.DPORT.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+
+    let mut rtc = Rtc::new(peripherals.RTC_CNTL);
+    let timer_group0 = TimerGroup::new(
+        peripherals.TIMG0,
+        &clocks,
+        &mut system.peripheral_clock_control,
+    );
+    let mut wdt = timer_group0.wdt;
+    
+    // Disable MWDT and RWDT (Watchdog) flash boot protection
+    wdt.disable();
+    rtc.rwdt.disable();
     
     println!("Hello world!");
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
-    let mut led = io.pins.gpio7.into_push_pull_output();
+    let mut led = io.pins.gpio2.into_push_pull_output();
     
     led.set_high().unwrap();
     let mut delay = Delay::new(&clocks);
 
     loop {
+        println!("Toggle");
         led.toggle().unwrap();
-        delay.delay_ms(500u32);
+        delay.delay_ms(1000u32);
     }
 }
